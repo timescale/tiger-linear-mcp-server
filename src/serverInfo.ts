7261a06eafce08513e8d640814a795ea848bb331
Greg Saab <greg@timescale.com>
2025-08-26 15:45:18 -0400

    fetch users and projects on init

--- src/serverInfo.ts
+++ src/serverInfo.ts
@@ -5,6 +5,7 @@ import { User } from './utils/user.js';
 import { Project } from './utils/project.js';
 import { getUsers } from './utils/getUsers.js';
 import { getProjects } from './utils/getProjects.js';
+import { log } from './shared/boilerplate/src/logger.js';
 
 export const serverInfo = {
   name: 'tiger-linear',
@@ -22,5 +23,11 @@ const linear = new LinearClient({
 
 const userStore = new Store<User>({ fetch: () => getUsers(linear) });
 const projectStore = new Store<Project>({ fetch: () => getProjects(linear) });
+
+projectStore
+  .get()
+  .catch((e) => log.error('Failed to fetch projects on init', e));
+
+userStore.get().catch((e) => log.error('Failed to fetch users on init', e));
 
 export const context: ServerContext = { linear, userStore, projectStore };
