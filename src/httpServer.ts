7f18cb0fec760fafc374388da29b7dfa246766ff
Matthew Peveler <mpeveler@tigerdata.com>
2025-10-06 12:20:51 -0600

    chore: replace mcp-boilerplate submodule with npm package
    Signed-off-by: Matthew Peveler <mpeveler@tigerdata.com>

--- src/httpServer.ts
+++ src/httpServer.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
+import { httpServerFactory } from '@tigerdata/mcp-boilerplate';
 import { apiFactories } from './apis/index.js';
-import { httpServerFactory } from './shared/boilerplate/src/httpServer.js';
 import { context, serverInfo } from './serverInfo.js';
 
 export const { registerCleanupFn } = httpServerFactory({
