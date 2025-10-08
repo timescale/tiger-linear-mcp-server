75689611d1003c216ad30ca3134f21e44ab332c0
Greg Saab <greg@timescale.com>
2025-10-08 07:56:42 -0400

    add a ttl to store

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,17 +1,26 @@
 interface StoreProps<T> {
   contents?: T;
   fetch: () => Promise<T[]>;
+  ttl?: number; // amount of time in milliseconds after which cached data will be considered stale
 }
 
 export class Store<T> {
   private contents: Promise<T[]> | null = null;
   private fetch: StoreProps<T>['fetch'];
+  private expirationDateTime?: number;
 
-  constructor({ fetch }: StoreProps<T>) {
+  constructor({ fetch, ttl }: StoreProps<T>) {
     this.fetch = fetch;
+
+    if (ttl) {
+      this.expirationDateTime = Date.now() + ttl;
+    }
   }
 
   async get(): Promise<T[]> {
+    if (this.expirationDateTime && Date.now() > this.expirationDateTime) {
+      this.contents = null;
+    }
     this.contents ??= this.fetch();
 
     return this.contents;
