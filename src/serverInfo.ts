04c64a8073c7a4887b04b344ef3adaf89954e5df
Greg Saab <greg@timescale.com>
2025-10-08 07:59:15 -0400

    set ttl for caches

--- src/serverInfo.ts
+++ src/serverInfo.ts
@@ -7,6 +7,9 @@ import { getUsers } from './utils/getUsers.js';
 import { getProjects } from './utils/getProjects.js';
 import { log } from '@tigerdata/mcp-boilerplate';
 
+const USER_CACHE_TTL = 1000 * 60 * 60; // one hour
+const PROJECTS_CACHE_TTL = 1000 * 60 * 60; // one hour
+
 export const serverInfo = {
   name: 'tiger-linear',
   version: '1.0.0',
@@ -21,8 +24,14 @@ const linear = new LinearClient({
   apiKey,
 });
 
-const userStore = new Store<User>({ fetch: () => getUsers(linear) });
-const projectStore = new Store<Project>({ fetch: () => getProjects(linear) });
+const userStore = new Store<User>({
+  fetch: () => getUsers(linear),
+  ttl: USER_CACHE_TTL,
+});
+const projectStore = new Store<Project>({
+  fetch: () => getProjects(linear),
+  ttl: PROJECTS_CACHE_TTL,
+});
 
 projectStore
   .get()
